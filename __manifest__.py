abeb0f2342c72fa7b7d0ecc557e487339631f01c
brahim94 <brahim-ayad@hotmail.com>
2020-12-17 14:50:11 -0800

    five

--- __manifest__.py
+++ __manifest__.py
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.4',
+    'version': '0.5',
 
     "external_dependencies": {
         'python': ['num2words']
