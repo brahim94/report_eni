87388a764ef2d731ade84e6a359e08095b15d7ba
brahim94 <brahim-ayad@hotmail.com>
2020-12-24 15:37:46 -0800

    delema

--- models/purchase_inherit.py
+++ models/purchase_inherit.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api, _ 
+from odoo import models, fields, api, _
+from num2words import num2words
 
 
 class tech_reports_extention(models.Model):
@@ -16,7 +17,8 @@ class tech_reports_extention(models.Model):
     ]
 
     state_bc_order = fields.Selection(PURCHASE_ORDER_BC_STATE, compute='_set_bc_order_state')
-
+    lot_number = fields.Char('N° de lot')
+    
     def action_fournisseur_choisi(self):
         return self.write({'state': "done"})
 
@@ -51,30 +53,12 @@ class tech_order_line(models.Model):
         return super(tech_order_line, self).create(vals)
 
 
-# class tech_sale_order(models.Model):
-#     _inherit = 'sale.order'
-    
-#     def print_engagement(self):
-#         return self.env.ref('tech_reports_extention.action_report_engagement').report_action(self)
-    
-#     def print_execution(self):
-#         return self.env.ref('tech_reports_extention.action_report_execution').report_action(self)
-
 class purchase_riquisition(models.Model):
     _inherit = 'purchase.requisition'
 
     def print_consultant_report(self):
         return self.env.ref('tech_reports_extention.action_report_examen').report_action(self)
 
-    def print_appel_offre(self):
-        return self.env.ref('tech_reports_extention.action_report_appel_offre').report_action(self)
-
-    def print_appel_offre_ar(self):
-        return self.env.ref('tech_reports_extention.action_report_appel_offre_ar').report_action(self)
-    
-    def print_engagement_ap_of(self):
-        return self.env.ref('tech_reports_extention.action_report_engagmnt').report_action(self)
-    
     def print_act_engagement(self):
         return self.env.ref('tech_reports_extention.action_report_act_eng').report_action(self)
  
@@ -111,6 +95,56 @@ class purchase_riquisition(models.Model):
     def print_prestation(self):
        return self.env.ref('tech_reports_extention.action_report_prestation').report_action(self)
 
+    montant_ttc_marche = fields.Float(string='Montant TTC', compute='_total_lot_all')
+
+    def _total_lot_all(self):
+        for lot in self:
+            montant_ttc_marche  = 0.0
+            for line in lot.lot_details_ids:
+                montant_ttc_marche += line.estimated_price
+            lot.update({
+                'montant_ttc_marche': montant_ttc_marche,
+            })    
+    
+    text_amount = fields.Char(string="Montant en lettre", required=False, compute="onchange_amount_total" )
+
+    @api.onchange('montant_ttc_marche')
+    def onchange_amount_total(self):
+        self.text_amount = num2words(self.montant_ttc_marche, lang='ar_AR')
+
+    def create_purchase_quotation(self):
+        purchase_order = self.env['purchase.order']
+        purchase_order_lst = []
+        purchase_order_ids = self.env['purchase.order'].search([('requisition_id', '=', self.id), ('state', 'in', ['draft', 'sent', 'to_approve'])])
+        purchase_order_ids.button_cancel()
+        purchase_order_ids.unlink()
+        self.create_decision_final_lines()
+        for each in self.tender_decision_ids:
+            product_lst = []
+            lot_line_ids = self.lot_details_ids.filtered(lambda l: l.id == each.lot_id.id)
+            for lot_line in lot_line_ids:
+                for rec in lot_line.product_ids:
+                    product_line = self.line_ids.filtered(lambda l: l.product_id.id == rec.id)
+                    product_lst.append((0, 0, {'product_id': rec.id,
+                                                'name': rec.name,
+                                                'product_qty': product_line[0].product_qty if product_line else 1.0,
+                                                'product_uom': rec.uom_id.id,
+                                                'price_unit': product_line[0].price_unit if product_line else 0.00,
+                                                'date_planned': product_line[0].schedule_date if product_line and product_line[0].schedule_date else fields.Date.today(),
+                                                'purchase_request_line_ids': [(6, 0, product_line.purchase_request_line_ids.ids)]}))
+            purchase_id = purchase_order.create({'partner_id': each.bidder_id.id,
+                                                  'partner_ref': (self.tender_number or '') + '-' + str(each.lot_id.n_lot) + ('-' + each.lot_id.object_name if each.lot_id.object_name else ''),
+                                                  'lot_number': str(each.lot_id.n_lot) + ('-' + each.lot_id.object_name if each.lot_id.object_name else ''),
+                                                  'requisition_id': self.id,
+                                                  'tender_decision_line_id': each.id,
+                                                  'order_line': product_lst})
+            purchase_order_lst.append(purchase_id.id)
+        
+        action = self.env.ref('purchase_requisition.action_purchase_requisition_list').read()[0]
+        action['domain'] = [('requisition_id', '=', self.id)]
+        action['context'] = {'default_requisition_id': self.id, 'default_user_id': False, 'default_partner_ref': self.tender_number, 'search_default_group_partner_ref': 1}
+        return action
+
 class market_execution(models.Model):
     _inherit = 'market.execution'
 
@@ -137,6 +171,10 @@ class market_execution(models.Model):
     def print_b_cmd(self):
         return self.env.ref('tech_reports_extention.action_report_b_cmd').report_action(self)
 
+    def print_engagement_ap_of(self):
+        return self.env.ref('tech_reports_extention.action_report_engagmnt').report_action(self)
+
+            
 class res_partner(models.Model):
     _inherit = 'res.partner'
 
@@ -146,3 +184,22 @@ class res_company(models.Model):
     _inherit = 'res.company'
 
     fax = fields.Char('Fax')
+
+
+class NewsPaper(models.Model):
+    _inherit = 'newspaper.publication'
+
+    def print_report_avis(self):
+        for pack in self:
+            for line in pack.requisition_id.newspaper_publication_ids:
+                if line.notice_type == 'français':
+                    return self.env.ref('tech_reports_extention.action_report_appel_offre').report_action(self)
+                if line.notice_type == 'Arabe':
+                    return self.env.ref('tech_reports_extention.action_report_appel_offre_ar').report_action(self)
+            
+    @api.depends('requisition_id.montant_ttc_marche')
+    def get_amount_in_words(self):
+        amount_in_words = self.requisition_id.currency_id.montant_ttc_marche(self.requisition_id.montant_ttc_marche)
+        return amount_in_words
+
+    
